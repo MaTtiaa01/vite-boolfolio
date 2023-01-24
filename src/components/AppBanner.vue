a9bb216b2aeba3061976e5265ff71f812d9fdf2f
MaTtiaa01 <mattia2001@live.it>
2023-01-24 17:44:36 +0100

    change banner

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -6,20 +6,39 @@ export default {
 
 
 <template>
-    <div class="container-fluid">
-    </div>
+    <section class="jumbotron container-fluid">
+        <div class="row row-cols-2 align-items-center">
+            <div class="col image">
+            </div>
+            <div class="col">
+                <h1 class="text-center">Full Stack Developer</h1>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 
 
 <style lang="scss" scoped>
-.container-fluid {
-    height: 350px;
-    margin: 0;
-    padding: 0;
-    background-image: url('../assets/img/desktop.jpeg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+.jumbotron {
+    height: 300px;
+
+    .col.image {
+        height: 300px;
+        background-image: url('../assets/img/construct.jpg');
+        background-position: center;
+    }
+
+    .col {
+        h1 {
+            color: white;
+            background-color: black;
+            border: 5px solid rgb(150, 1, 1);
+            padding: 1rem;
+            width: 40%;
+
+        }
+    }
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,17 @@
 <script>
+import AppBanner from '../components/AppBanner.vue';
+
 export default {
-    name: 'AboutView'
+    name: 'AboutView',
+    components: {
+        AppBanner
+    }
 }
 </script>
 
 
 <template>
+    <AppBanner></AppBanner>
     <h1>about</h1>
 
 </template>
