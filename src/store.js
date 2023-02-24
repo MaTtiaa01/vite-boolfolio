a584aa599be290bf19c4fdd58723ed19067ce6c9
MaTtiaa01 <mattia2001@live.it>
2023-02-24 18:16:57 +0100

    change base api url

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    base_api_url: 'https://mattiadev.com',
+    base_api_url: 'https://admin.mattiadev.com',
     getImagePath(path) {
         if (path) {
             return this.base_api_url + '/storage/' + path
